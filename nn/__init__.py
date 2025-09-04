2e1cbdbc00c4902ccc3345ac43e053eebc29723b
Sreehari S J <isrosreehari@gmail.com>
2025-09-04 14:37:01 +0530

    Add __init__.py to make nn a package

---
+++ nn/__init__.py
@@ -0,0 +1,8 @@
+# This file makes the 'nn' folder a Python package.
+# You can leave it empty or define package-level imports.
+# Optional: import main modules for easier access
+
+from .activations import *
+from .layers import *
+from .utils import *
+from .model import HeartRiskNN
